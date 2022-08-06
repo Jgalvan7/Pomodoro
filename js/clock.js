6728a76ff17788399e1ebf947bcda1688998a1a6
Jesus Galvan Gonzalez <71811268+Jgalvan7@users.noreply.github.com>
2022-08-06 10:55:28 +0100

    Subimos archivos del repositorio local

---
+++ js/clock.js
@@ -0,0 +1,53 @@
+const ClockApp = document.querySelector(".app_clock");
+const FullTime = document.createElement("div");
+FullTime.id = "clockDisplay";
+ClockApp.append(FullTime);
+
+const clockDisplayHours = document.createElement("span");
+clockDisplayHours.id = "hours";
+const clockDisplayMinutes = document.createElement("span");
+clockDisplayMinutes.id = "minutes";
+const clockDisplaySeconds = document.createElement("span");
+clockDisplaySeconds.id = "seconds";
+const point = ":";
+FullTime.append(clockDisplayHours,point,clockDisplayMinutes,point,clockDisplaySeconds);
+
+// Reloj
+let clockDisplay = setInterval(() => {
+    let timeCount = new Date();
+    clockDisplayHours.textContent = formatValue(timeCount.getHours());
+    clockDisplayMinutes.textContent = formatValue(timeCount.getMinutes());
+    clockDisplaySeconds.textContent = formatValue(timeCount.getSeconds());
+}, 10);
+
+// Alarma
+const AlarmContainer = document.createElement("div");
+AlarmContainer.id = "alarmContainer";
+ClockApp.append(AlarmContainer);
+
+function setAlarm() {
+    let alarmInput = document.createElement("input");
+    alarmInput.type = "time";
+    alarmInput.id = "setAlarm";
+    AlarmContainer.append(alarmInput);
+}
+setAlarm();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Funcona para dar formato de dos digitos a la hora.
+function formatValue(value) {
+    return ("0" + value).slice(-2);
+}
