75fec22c2278956de7fccb3952ec23a792b2be34
Jgalvan <galvan.jgg@gmail.com>
2022-08-07 12:48:58 +0100

    Se crean los botones en el documento JS para interatuar con la alarma, se da funcionalidad al boton de reset de la alarma

--- js/clock.js
+++ js/clock.js
@@ -24,17 +24,43 @@ let clockDisplay = setInterval(() => {
 const AlarmContainer = document.createElement("div");
 AlarmContainer.id = "alarmContainer";
 ClockApp.append(AlarmContainer);
+AlarmContainer.addEventListener("click", (event) => {
+    if(event.target.id == "setAlarm") {
+        hourAlarm = event.target.value;
+        if(hourAlarm != "") {
+            avaiableAlarm();
+        }
+    }
+});
+
 
 function setAlarm() {
     let alarmInput = document.createElement("input");
     alarmInput.type = "time";
     alarmInput.id = "setAlarm";
-    AlarmContainer.append(alarmInput);
+    let btnActive = document.createElement("button");
+    btnActive.id = "btnActiveAlarm";
+    btnActive.textContent = "Activar";
+    let btnDisable = document.createElement("button");
+    btnDisable.id = "btnDisableAlarm";
+    btnDisable.textContent = "Desactivar";
+    let btnReset = document.createElement("button");
+    btnReset.id = "btnResetAlarm";
+    btnReset.textContent = "Resetear";
+    btnReset.onclick = () => {
+        resetAlarm();
+    };
+    AlarmContainer.append(alarmInput,btnActive,btnDisable,btnReset);
 }
 setAlarm();
-
+const AlarmActive = document.querySelector("#setAlarm");
 function avaiableAlarm() {
-    
+    console.log("Alarma Activa");
+}
+function disableAlarm() {
+}
+function resetAlarm() {
+    AlarmActive.value = "";
 }
 
 
